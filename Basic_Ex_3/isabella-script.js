f2c2f82e00fc7b7ca5abdd3c013751c793ef05e9
Isabella W <isabellawindhaber@gmail.com>
2022-10-05 15:26:37 +0200

    ...

--- Basic_Ex_3/isabella-script.js
+++ Basic_Ex_3/isabella-script.js
@@ -3,4 +3,28 @@
 
 1. Print only the index of the elements.
 2. Print only the values.
-3. Print both index and values. */ 
+3. Print both index and values. */
+const arr = ["isabella", "mark", "lisa", "tim"];
+console.log(arr);
+//Print only the index of the elements
+function printIndex() {
+    for (let i in arr) {
+        console.table(i);
+        document.write(i);
+    }
+}
+printIndex();
+// Print only the values
+function printValues() {
+    for (let i of arr) {
+        console.table(i);
+        document.write(i);
+    }
+}
+printValues();
+// 3. Print both index and values
+function printBoth() {
+    printIndex();
+    printValues();
+}
+printBoth();
--- Basic_Ex_3/isabella-script.ts
+++ Basic_Ex_3/isabella-script.ts
@@ -3,3 +3,34 @@
 1. Print only the index of the elements.
 2. Print only the values. 
 3. Print both index and values. */
+
+const arr: string[] = ["isabella", "mark", "lisa", "tim"]
+console.log(arr);
+
+//Print only the index of the elements
+function printIndex() {
+    for (let i in arr){
+        console.table(i);
+        document.write(i);
+    }
+}
+printIndex();
+
+
+// Print only the values
+function printValues() {
+for (let i of arr) {
+    console.table(i);
+    document.write(i);
+}
+}
+printValues();
+
+// 3. Print both index and values
+function printBoth() {
+    printIndex();
+    printValues();
+}
+
+printBoth();
+
