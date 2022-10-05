f5be75eff28220d23152cccbc81dbf69eec919fa
Isabella W <isabellawindhaber@gmail.com>
2022-10-05 15:04:04 +0200

    .

--- Basic_Ex_2/isabella-script.ts
+++ Basic_Ex_2/isabella-script.ts
@@ -2,11 +2,24 @@
 2. Print your first name 10 times in the browser using a forEach loop (don't forget that forEach loop work on arrays, so you can put the object in an array and repeat it). 
 3. A function should be triggered 5 seconds after opening the page that prints your last name in the console 10 times.*/
 
-const obj = {firstName: "Isabella", lastName: "Windhbaer"}
+const obj = [{firstName: "Isabella", lastName: "Windhbaer"}]
 
 console.log(obj);
 
-document.querySelectorAll("#name").innerHTML = obj.firstName;
+let br = "<br>";
+
+obj.forEach(obj => document.write(obj.firstName));
+obj.forEach(obj => document.write(obj.firstName));
+obj.forEach(obj => document.write(obj.firstName));
+br
+obj.forEach(obj => document.write(obj.firstName));
+br
+obj.forEach(obj => document.write(obj.firstName));
+br
+obj.forEach(obj => document.write(obj.firstName));
+
+
+
 
 
 // obj.forEach(element => document.write(element.firstName));
