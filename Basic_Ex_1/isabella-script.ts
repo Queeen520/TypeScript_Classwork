bcd852b9ede86d30c31160d27f83c5ccef1af4e7
Isabella W <isabellawindhaber@gmail.com>
2022-10-05 12:49:39 +0200

    ..

--- Basic_Ex_1/isabella-script.ts
+++ Basic_Ex_1/isabella-script.ts
@@ -2,10 +2,12 @@
 
 let array: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-for (let i = 0; i <= array.length; i++) {
 
-    if (i < array.length) {
-        let result = 1*array[i];
-        console.log(result);
-    } 
+for (let i = 1; i <= array.length; i++){
+    console.log(i + " x 1 = " + i);
 }
+
+for (let i = 1; i <= array.length; i++){
+    console.log(i + " x 2 = " + i*2);
+}
+
